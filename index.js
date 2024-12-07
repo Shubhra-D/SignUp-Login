3bd3e3519fabb587f06b98f9f5320704d7c3a46f
Shubhra D <shubhradwivedi21@gmail.com>
2024-12-07 20:19:48 +0530

    Second code

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ function signup(userName){
       return "Signup Successfull ,Please Login" 
 }
 console.log(signup("Koko"));
+let correct="Emp@123"
+function login(userName,password){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]==userName&&correct==password){
+            return "Login Successfull";
+        }else if(arr[i]!==userName) {
+            return "User Not Found,Please Signup"
+        }else if(arr[i]==userName&&correct!==password){
+            return "Wrong password"
+        }
+    }
+        
+}
+console.log(login("Alice","zae"));
